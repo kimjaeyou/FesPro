e9425dfbdc38f0d7342e0d0330d952482a498596
iylee <iylee153@gmail.com>
2024-10-11 18:26:06 +0900

    10/11 18:25 reservation service, dao 추가 - 인영

--- src/main/java/wep/mvc/dto/ReservationDTO.java
+++ src/main/java/wep/mvc/dto/ReservationDTO.java
@@ -1,116 +1,115 @@
 package wep.mvc.dto;
 
 public class ReservationDTO {
-	private int reserv_seq;
-	private int user_seq;
+	private int reservSeq;
+	private int userSeq;
 	private String SVCID; 
-	private String resv_date; 
-	private String svc_date;
-	private String svc_time;
-	private int resv_peo;
-	private int resv_price;
-	private int resv_check;
+	private String resvDate; 
+	private String svcDate;
+	private String svcTime;
+	private int resvPeo;
+	private int resvPrice;
+	private int resvCheck;
 	
 	public ReservationDTO () {
 		
 	}
 	
-	public ReservationDTO(int reserv_seq, int user_seq, String sVCID, String svc_date, String svc_time, int resv_peo,
-			int resv_price) {
+	public ReservationDTO(int userSeq, String sVCID, String svcDate, String svcTime, int resvPeo,
+			int resvPrice) {
 		super();
-		this.reserv_seq = reserv_seq;
-		this.user_seq = user_seq;
-		SVCID = sVCID;
-		this.svc_date = svc_date;
-		this.svc_time = svc_time;
-		this.resv_peo = resv_peo;
-		this.resv_price = resv_price;
+		this.userSeq = userSeq;
+		this.SVCID = sVCID;
+		this.svcDate = svcDate;
+		this.svcTime = svcTime;
+		this.resvPeo = resvPeo;
+		this.resvPrice = resvPrice;
 	}
 	
-	public ReservationDTO(int reserv_seq, int user_seq, String sVCID, String resv_date, String svc_date, String svc_time,
-			int resv_peo, int resv_price, int resv_check) {
+	public ReservationDTO(int reservSeq, int userSeq, String sVCID, String resvDate, String svcDate, String svcTime,
+			int resvPeo, int resvPrice, int resvCheck) {
 		super();
-		this.reserv_seq = reserv_seq;
-		this.user_seq = user_seq;
-		SVCID = sVCID;
-		this.resv_date = resv_date;
-		this.svc_date = svc_date;
-		this.svc_time = svc_time;
-		this.resv_peo = resv_peo;
-		this.resv_price = resv_price;
-		this.resv_check = resv_check;
+		this.reservSeq = reservSeq;
+		this.userSeq = userSeq;
+		this.SVCID = sVCID;
+		this.resvDate = resvDate;
+		this.svcDate = svcDate;
+		this.svcTime = svcTime;
+		this.resvPeo = resvPeo;
+		this.resvPrice = resvPrice;
+		this.resvCheck = resvCheck;
 	}
-	
-	public int getReserv_seq() {
-		return reserv_seq;
+
+	public int getReservSeq() {
+		return reservSeq;
 	}
-	
-	public void setReserv_seq(int reserv_seq) {
-		this.reserv_seq = reserv_seq;
+
+	public void setReservSeq(int reservSeq) {
+		this.reservSeq = reservSeq;
 	}
-	
-	public int getUser_seq() {
-		return user_seq;
+
+	public int getUserSeq() {
+		return userSeq;
 	}
-	
-	public void setUser_seq(int user_seq) {
-		this.user_seq = user_seq;
+
+	public void setUserSeq(int userSeq) {
+		this.userSeq = userSeq;
 	}
-	
+
 	public String getSVCID() {
 		return SVCID;
 	}
-	
+
 	public void setSVCID(String sVCID) {
 		SVCID = sVCID;
 	}
-	
-	public String getResv_date() {
-		return resv_date;
+
+	public String getResvDate() {
+		return resvDate;
 	}
-	
-	public void setResv_date(String resv_date) {
-		this.resv_date = resv_date;
+
+	public void setResvDate(String resvDate) {
+		this.resvDate = resvDate;
 	}
-	
-	public String getSvc_date() {
-		return svc_date;
+
+	public String getSvcDate() {
+		return svcDate;
 	}
-	
-	public void setSvc_date(String svc_date) {
-		this.svc_date = svc_date;
+
+	public void setSvcDate(String svcDate) {
+		this.svcDate = svcDate;
 	}
-	
-	public String getSvc_time() {
-		return svc_time;
+
+	public String getSvcTime() {
+		return svcTime;
 	}
-	
-	public void setSvc_time(String svc_time) {
-		this.svc_time = svc_time;
+
+	public void setSvcTime(String svcTime) {
+		this.svcTime = svcTime;
 	}
-	
-	public int getResv_peo() {
-		return resv_peo;
+
+	public int getResvPeo() {
+		return resvPeo;
 	}
-	
-	public void setResv_peo(int resv_peo) {
-		this.resv_peo = resv_peo;
+
+	public void setResvPeo(int resvPeo) {
+		this.resvPeo = resvPeo;
 	}
-	
-	public int getResv_price() {
-		return resv_price;
+
+	public int getResvPrice() {
+		return resvPrice;
 	}
-	
-	public void setResv_price(int resv_price) {
-		this.resv_price = resv_price;
+
+	public void setResvPrice(int resvPrice) {
+		this.resvPrice = resvPrice;
 	}
-	
-	public int getResv_check() {
-		return resv_check;
+
+	public int getResvCheck() {
+		return resvCheck;
 	}
-	
-	public void setResv_check(int resv_check) {
-		this.resv_check = resv_check;
+
+	public void setResvCheck(int resvCheck) {
+		this.resvCheck = resvCheck;
 	}
 	
 	
